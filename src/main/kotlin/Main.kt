6997492c355cd4f455dcd32951ee021a41108e5b
Calinoiu Alexandru Nicolae <calinoiu.alexandru@agilefreaks.com>
2019-01-26 08:39:18 +0200

    Add assertion contract

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,17 @@
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
+
+@ExperimentalContracts
+fun Int?.isValid(): Boolean {
+    contract {
+        returns(true)  implies (this@isValid != null)
+    }
+
+    return this != null && this != 0
+}
+
 class Main {
+    fun getMyToken() : String? {
+        return "something"
+    }
 }
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,9 +1,31 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
 
 internal class MainTest {
+    private val main by lazy {
+        Main()
+    }
+
+    @ExperimentalContracts
     @Test
     fun `will most likely work`() {
-        assertEquals(true, true)
+        val token = main.getMyToken()
+
+        assertNotNull(token)
+        assertEquals(42, token.length)
     }
+}
+
+@ExperimentalContracts
+fun assertTrue(actual: Boolean, message: String? = null) {
+    contract { returns() implies actual }
+    org.junit.jupiter.api.Assertions.assertTrue(actual, message)
+}
+
+@ExperimentalContracts
+fun <T : Any> assertNotNull(actual: T?, message : String? = null) {
+    contract { returns() implies (actual != null) }
+    org.junit.jupiter.api.Assertions.assertNotNull(actual, message)
 }
