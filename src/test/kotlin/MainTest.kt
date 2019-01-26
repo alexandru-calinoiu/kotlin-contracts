ca4db67e7e535c2dec42860269ab90118d7e102b
Calinoiu Alexandru Nicolae <calinoiu.alexandru@agilefreaks.com>
2019-01-26 06:52:55 +0200

    Initial commit

---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class MainTest {
+    @Test
+    fun `will most likely work`() {
+        assertEquals(true, true)
+    }
+}
